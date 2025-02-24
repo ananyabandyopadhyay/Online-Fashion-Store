fdc60a8a2977e54ee41265dad590616d4c0f4c40
Ananya Bandyopadhyay <ani99banerjee@gmail.com>
2025-02-24 12:37:36 +0530

    production

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar() {
   return (
     <nav>
       <span className="web_sprite_logo brand-logo" onClick={navHome}></span>
-      <>production</>
+      <>productionss</>
       <div className="nav-actions">
         <div className="search-bar">
           <div className="search-btn" onClick={search}>
